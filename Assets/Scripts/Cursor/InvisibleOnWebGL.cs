a0ed1e2fb1be95b805104cd0c2429570a4c74af8
Avi Vajpeyi <avi.vajpeyi@gmail.com>
2019-07-20 03:09:51 +1000

    added invisible on webgl

---
+++ Assets/Scripts/Cursor/InvisibleOnWebGL.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InvisibleOnWebGL : MonoBehaviour
+{
+
+    void Start()
+    {
+
+        if (Application.platform == RuntimePlatform.Android)
+        {
+            gameObject.SetActive(false);
+        }
+    }
+}
